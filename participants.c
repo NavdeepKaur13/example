41319cbcd903dc32d2567852954309810987ef8b
Navdeep <navdeepkaur2021081@gmail.com>
2023-04-11 14:12:50 +0530

    Added information about Navdeep Kaurt

--- participants.c
+++ participants.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 void main(){
-
+    printf("Name : Navdeep Kaur \nClass : D3ITB1 \nRoll No. : 2004960/2021081 ");
     printf("Name : Armaan Singh \nClass : D3ITA1 \nRoll No. : 2004893/2021134 ");
 }
